c49c3dd8d56befcde4f4d8a1069de91b7ca4e3a6
Isaac Yu <kan_yu@manulife.ca>
2020-01-24 14:23:08 -0500

    Completed module

--- src/app.js
+++ src/app.js
@@ -54,7 +54,7 @@ app.post('/payment',(req,res)=>{
   accounts.credit.available+=parseInt(req.body.amount,10);
   const accountsJSON=JSON.stringify(accounts,null,4);
   fs.writeFileSync(path.join(__dirname,'json','accounts.json'),accountsJSON,'utf8');
-  res.render('transfer',{message:'Payment Successful',account:accounts.credit});
+  res.render('payment',{message:'Payment Successful',account:accounts.credit});
 });
 
 
