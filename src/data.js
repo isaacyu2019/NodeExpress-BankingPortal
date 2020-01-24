aa1595c0815cf91ae64e28f7167150b0be1382a0
Isaac Yu <kan_yu@manulife.ca>
2020-01-24 14:38:57 -0500

    Completed module

--- src/data.js
+++ src/data.js
@@ -15,3 +15,5 @@ const writeJSON = () => {
   const accountsJSON = JSON.stringify(accounts, null, 4);
   fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
 }
+
+module.exports={accounts,users,writeJSON};
